05ea0e05eae5f60afaec5e92cbb3f43004a004c8
Jean Lopes <jeean.llopes@outlook.com>
2018-02-25 23:44:47 -0300

    Add project

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ExceptionsLibrary;
+using System;
 
 namespace ConsoleUI
 {
@@ -10,7 +7,10 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            a
+            DemoCode demoCode = new DemoCode();
+            Console.WriteLine($"this value at the givem position is {demoCode.GrandParentMethod(3)}");
+            Console.ReadLine();
+
         }
     }
 }
---
+++ ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsLibrary
+{
+    public class DemoCode
+    {
+        public int GrandParentMethod(int position)
+        {
+            return ParentMethod(position);
+        }
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = 0;
+            int[] numbers = new int[] { 1, 4, 7, 2 };
+            output = numbers[position];
+
+            return output;
+        }
+    }
+}
